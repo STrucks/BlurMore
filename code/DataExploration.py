bbf8865b2163f1c06520fb3044839ae224d336a1
STrucks <31651504+STrucks@users.noreply.github.com>
2019-04-03 16:17:40 +0200

    Add files via upload

--- code/DataExploration.py
+++ code/DataExploration.py
@@ -40,5 +40,50 @@ def show_user_item_matrix():
     plt.title("difference between these two")
     plt.show()
 
-histogram()
+
+def gender_rating_distr():
+    frequencies_male = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    }
+    frequencies_female = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    }
+    user_gender = {}
+    with open("ml-1m/users.dat", 'r') as f:
+        for line in f.readlines():
+            user_id, gender, _, _, _ = line.split("::")
+            user_gender[user_id] = gender
+
+    with open("ml-1m/ratings.dat", 'r') as f:
+        for line in f.readlines():
+            user_id, movie_id, rating, _ = line.split("::")
+            rating = int(rating)
+            if user_gender[user_id] == 'M':
+                frequencies_male[rating] += 1
+            elif user_gender[user_id] == 'F':
+                frequencies_female[rating] += 1
+    print(frequencies_male)
+    sum_male = sum(frequencies_male.values())
+    propotion_male = [value / sum_male for value in frequencies_male.values()]
+    sum_female = sum(frequencies_female.values())
+    propotion_female = [value / sum_female for value in frequencies_female.values()]
+
+    from matplotlib import pyplot as plt
+    plt.plot(propotion_male, label="propotion male")
+    plt.plot(propotion_female, label="propotion female")
+    plt.xticks([0,1,2,3,4], [1,2,3,4,5])
+    plt.xlabel("rating")
+    plt.ylabel("Percent of ratings")
+    plt.legend()
+    plt.show()
+gender_rating_distr()
+#histogram()
 #show_user_item_matrix()
